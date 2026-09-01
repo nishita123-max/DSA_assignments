724f8cf019837a467eb784bee7e99d8c27d917c7
nishita123-max <nishitadutta10tha@gmail.com>
2026-09-01 10:34:33 +0530

    Create 5(d).cpp

---
+++ Assignment2/5(d).cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the size of matrix(n*n) : ";
+    cin>>n;
+    int size = (n*(n+1))/2;
+    int a[size];
+    cout<<"Enter the elements (n*(n+1))/2: ";
+    for(int i=0; i<size; i++)
+    {
+        cin>>a[i];
+    }
+    int curr = 0;
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            if(i <= j)
+            {
+                cout<<a[curr]<<" ";
+                curr++;
+            }
+            else{
+                cout<<"0"<<" ";
+            }
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
